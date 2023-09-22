b4de1497462e04345907597a279b65950284ba59
ADAM BALLANTYNE <a.ballantyne.22@abdn.ac.uk>
2023-09-22 13:10:13 +0000

    Second Commit(Division)

---
+++ Java Swing Turtle/turtleman.java
@@ -0,0 +1,8 @@
+public class turtleman{
+  public static void main(String[] args){
+    World world = new World(150, 150);
+    Turtle yertle = new turtle(world);
+    world.show(true);
+  }
+
+}
--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 public class hello {
   public static void main(String[] args) {
     System.out.print("Hello World");
-    System.out.println(" you fatty fat fucks");
+    System.out.println(" my good pal");
   }
 }
